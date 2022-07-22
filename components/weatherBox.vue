e658df874c6af9509d2dd978f47468225c2d456d
julesprezrdlf <julioprezrdlf@gmail.com>
2022-07-22 21:30:03 +0200

    fixing

---
+++ components/weatherBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class ="weatherBoxContainer
+               mt-10 
+               col-start-2 col-end-12
+               sm:col-start-3 sm:col-end-11
+               lg:col-start-4 lg:col-end-10
+               ">
+
+
+      
+        
+        
+    <h1>London</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.weatherBoxContainer{
+     
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mainContainer grid grid-cols-12 ">
+    <WeatherBox/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
